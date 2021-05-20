366fb9feeb38960f9ddcff4dd55384948225113a
Will DeBernardi <will@debernardi.us>
2021-05-19 23:42:39 -0600

    Add basic board structure

---
+++ script.js
@@ -0,0 +1,23 @@
+const gameBoard = (() => {
+    // "e" represents empty board slot
+    let board = ["e", "e", "e", "e", "e", "e", "e", "e", "e"];
+
+    const updateBoard = (index, state) => {
+        if(state == "X") {
+            board[index] = "X";
+            console.log("x");
+        } else if (state == "O") {
+            board[index] = "O";
+        }
+    }
+
+    const printArray = () => {
+        for(let a of board) {
+            console.log(a);
+        }
+    }
+
+    return {board, updateBoard};
+})();
+
+gameBoard.updateBoard(1, "O");
