65baac9399061e654a38386b1f15c0e0044d52d3
Will DeBernardi <will@debernardi.us>
2021-05-20 23:45:39 -0600

    Add X/O alternation, occupied space checking, and a turn label

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ const displayController = (() => {
     const buttonContainer = document.getElementById("player-select");
     const xButton = document.getElementById("x-select");
     const oButton = document.getElementById("o-select");
+    const turnLabel = document.getElementById("turn-label");
 
     xButton.addEventListener("click", function() {
         gameBoard.playerState = "X";
@@ -65,12 +66,28 @@ const displayController = (() => {
     for (let square of squaresArray) {
         square.addEventListener("click", function() {
             let index = squaresArray.indexOf(square);
+            if(gameBoard.board[index] != "e") {
+                return;
+            }
+
             if(gameBoard.playerState == "X") {
                 gameBoard.board[index] = "X";
                 displayView.updateDisplay();
             } else if (gameBoard.playerState == "O") {
                 gameBoard.board[index] = "O";
                 displayView.updateDisplay();
+            }
+
+            if(gameBoard.playerState == "X") {
+                gameBoard.playerState = "O";
+            } else if (gameBoard.playerState == "O") {
+                gameBoard.playerState = "X";
+            }
+
+            if(turnLabel.innerHTML == "Player one's turn") {
+                turnLabel.innerHTML = "Player two's turn";
+            } else {
+                turnLabel.innerHTML = "Player one's turn";
             }
         });
     }
