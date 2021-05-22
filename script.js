1c8674ebf31889d0cf6f1c91f86a72387e91d85f
Will DeBernardi <will@debernardi.us>
2021-05-22 15:56:20 -0600

    Add win checking, and board restting which doesn't quite work yet

--- script.js
+++ script.js
@@ -18,14 +18,57 @@ const gameBoard = (() => {
         }
     }
 
-    return {board, updateBoard, playerState};
+    const checkWin = () => {
+        let winPos = {
+            rowOne: [board[0], board[1], board[2]],
+            rowTwo: [board[3], board[4], board[5]],
+            rowThree: [board[6], board[7], board[8]],
+            vertOne: [board[0], board[3], board[6]],
+            vertTwo: [board[1], board[4], board[7]],
+            vertThree: [board[2], board[5], board[8]],
+            diagonal: [board[0], board[4], board[8]]
+        }
+
+        let result = false;
+        let trueCounter = 0;
+        for(let property in winPos) {
+            trueCounter = 0;
+            for(let value in winPos[property]) {
+                // console.log([winPos[property]]);
+                if(winPos[property][value] == "X") {
+                    trueCounter++;
+                    if(trueCounter == 3) {
+                        result = true;
+                        break;
+                    }
+                } else if (winPos[property][value] == "O") {
+                    trueCounter++;
+                    if(trueCounter == 3) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    const resetBoard = () => {
+        let board = ["e", "e", "e", "e", "e", "e", "e", "e", "e"];
+        console.log("forst" + board);
+        playerState = "";
+        displayView.resetDisplay();
+    }
+
+    return {board, updateBoard, playerState, checkWin, resetBoard};
 })();
 
 const displayView = (() => {
     const squares = document.getElementsByClassName("board-slot")
     // Convert HTMLCollection to array to use indexOf
     const squaresArray = Array.from(squares);
-    // TODO: change square to be X/O rather than colors
+    const boardContainer = document.getElementById("board-container");
+    const buttonContainer = document.getElementById("player-select");
     const updateDisplay = () => {
         for(let square of squaresArray) {
             let index = squaresArray.indexOf(square);
@@ -39,7 +82,16 @@ const displayView = (() => {
         }
     }
 
-    return {updateDisplay}
+    const resetDisplay = () => {
+        for(let square of squaresArray) {
+            square.innerHTML = "";
+            square.classList.remove("x", "o");
+        }
+        boardContainer.classList.add("hidden");
+        buttonContainer.classList.remove("hidden");
+    }
+
+    return {updateDisplay, resetDisplay}
 })();
 
 const displayController = (() => {
@@ -53,6 +105,7 @@ const displayController = (() => {
 
     xButton.addEventListener("click", function() {
         gameBoard.playerState = "X";
+        console.log(gameBoard.playerState);
         buttonContainer.classList.add("hidden");
         boardContainer.classList.remove("hidden");
     });
@@ -73,9 +126,19 @@ const displayController = (() => {
             if(gameBoard.playerState == "X") {
                 gameBoard.board[index] = "X";
                 displayView.updateDisplay();
+                if(gameBoard.checkWin()) {
+                    // setTimeout(() => {alert("X wins!")}, 100);
+                    alert("x wins")
+                    gameBoard.resetBoard();
+                    console.log(gameBoard.board);
+                }
             } else if (gameBoard.playerState == "O") {
                 gameBoard.board[index] = "O";
                 displayView.updateDisplay();
+                if(gameBoard.checkWin()) {
+                    setTimeout(() => {alert("O wins!")}, 100);
+                    gameBoard.resetBoard();
+                }
             }
 
             if(gameBoard.playerState == "X") {
